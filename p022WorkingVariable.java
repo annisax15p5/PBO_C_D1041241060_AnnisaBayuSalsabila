1f091122f0d3f07cf7c3f6bd8c918680dd0378f3
annisax15p5 <annisabayusalsabila@gmail.com>
2026-02-13 21:33:49 +0700

    first commit

---
+++ p022WorkingVariable.java
@@ -0,0 +1,12 @@
+public class p022WorkingVariable {
+    public static void main (String[] args){
+        // Deklarasi dan inisialisasi variable
+        int angka = 10;
+        // Menampilkan nilai awal
+        System.out.println("Nilai awal angka = " + angka);
+        // Mengubah nilai variable
+        angka = 25;
+        // Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah = " + angka);
+    }
+}
